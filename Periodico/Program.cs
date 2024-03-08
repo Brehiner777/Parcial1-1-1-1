4cd941a8ebd9be31140fdd5489d931acb5e6fa81
Brehiner Eusse <Brehiner Eusse@DAV>
2024-03-08 08:24:52 -0500

    Add project files.

---
+++ Periodico/Program.cs
@@ -0,0 +1,13 @@
+﻿using Periodico;
+
+class Program
+{
+    static void Main()
+    {
+        var libro1 = new Publicacion(200, 10, "El Gran Libro", "lujo", "Autor Genial", "01/03/2024", false);
+        var periodico1 = new Publicacion(50, 5, "Diario Noticias", "normal", "Editor Jefe", "02/03/2024", true);
+
+        Console.WriteLine($"Costo del libro: ${libro1.CalcularCosto()}");
+        Console.WriteLine($"Costo del periódico: ${periodico1.CalcularCosto()}");
+    }
+}
---
+++ Periodico/Publicacion.cs
@@ -0,0 +1,61 @@
+﻿using Periodico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Periodico
+{
+    public class Publicacion
+    {
+        public int NumeroHojas { get; set; }
+        public double ValorHoja { get; set; }
+        public string Titulo { get; set; }
+        public string TipoPortada { get; set; }
+        public string NombreAutor { get; set; }
+        public string Fecha { get; set; }
+        public bool HojasColor { get; set; }
+
+        public Publicacion(int numHojas, double valorHoja, string titulo, string tipoPortada, string autor, string fecha, bool color)
+        {
+            NumeroHojas = numHojas;
+            ValorHoja = valorHoja;
+            Titulo = titulo;
+            TipoPortada = tipoPortada;
+            NombreAutor = autor;
+            Fecha = fecha;
+            HojasColor = color;
+        }
+
+        public double CalcularCosto()
+        {
+            double costo = NumeroHojas * ValorHoja;
+
+            switch (TipoPortada.ToLower())
+            {
+                case "lujo":
+                    costo += 15000;
+                    break;
+                case "normal":
+                    costo += 10000;
+                    break;
+                case "económica":
+                    costo += 5000;
+                    break;
+                default:
+                    Console.WriteLine("Tipo de portada no válido. Se asumirá portada normal.");
+                    costo += 10000;
+                    break;
+            }
+
+            if (HojasColor)
+            {
+                costo += 500 * NumeroHojas;
+            }
+
+            return costo;
+        }
+    }
+}
+
